05c67e230f5f257db18467ff9bea4d2e893cf353
Jesse <jesseabe.rodrigues@gmail.com>
2024-09-16 19:11:25 -0300

    update

--- 03-webscraping-competitive-intelligence/coleta/coleta/spiders/unisinos.py
+++ 03-webscraping-competitive-intelligence/coleta/coleta/spiders/unisinos.py
@@ -1,10 +1,17 @@
 import scrapy
 
-
 class UnisinosSpider(scrapy.Spider):
     name = "unisinos"
     allowed_domains = ["ead.unisinos.br"]
     start_urls = ["https://ead.unisinos.br/cursos-graduacao"]
 
     def parse(self, response):
-        pass
+        # Obtém todos os links dos cursos
+        cursos = response.css('a.course-link-card::attr(href)').getall()
+        
+        # Itera sobre cada curso
+        for curso in cursos:
+            yield {
+                'course': curso  # Aqui usamos 'curso' diretamente
+            }
+
