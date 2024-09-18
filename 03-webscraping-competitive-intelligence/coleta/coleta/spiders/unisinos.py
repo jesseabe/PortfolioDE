0b34fb2a88568d44dd535d711cbd29eb16807b2d
Jesse <jesseabe.rodrigues@gmail.com>
2024-09-17 23:15:38 -0300

    update - ta quase

--- 03-webscraping-competitive-intelligence/coleta/coleta/spiders/unisinos.py
+++ 03-webscraping-competitive-intelligence/coleta/coleta/spiders/unisinos.py
@@ -19,16 +19,18 @@ class UnisinosSpider(scrapy.Spider):
         # Extrai as informações desejadas de cada página do curso
         tipo_curso = response.css('span.subtitle[data-gtm-course-category="true"]::text').get()
         curso = response.css('h1[data-gtm-course-name="true"]::text').get()
-        mensalidade_original = response.css('p.mensalidade.original[data-gtm-course-price]::text').get()
-        mensalidade_promo = response.css('p.mensalidade.promo[data-gtm-course-price]::text').get()
-        duracao = response.css('span svg + span::text').get()  # Ajuste conforme o layout do site
+        mensalidade_original = response.css('p.mensalidade.original[data-gtm-course-price]::text').getall()
+        mensalidade_promo = response.css('p.mensalidade.promo[data-gtm-course-price]::text').getall()
+        duracao = response.css('span svg + span::text').getall()  # Ajuste conforme o layout do site
+        primeiro_mensalidade_original = mensalidade_original[3] if mensalidade_original else None
+        primeiro_mensalidade_promo = mensalidade_promo[3] if mensalidade_promo else None
 
         # Retorna as informações coletadas
         yield {
             'tipo_curso': tipo_curso,
             'curso': curso,
-            'mensalidade_original': mensalidade_original,
-            'mensalidade_promo': mensalidade_promo,
+            'mensalidade_original': primeiro_mensalidade_original,
+            'mensalidade_promo': primeiro_mensalidade_promo,
             'duracao': duracao
         }
 
