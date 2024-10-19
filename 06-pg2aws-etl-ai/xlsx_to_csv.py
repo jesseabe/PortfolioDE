4934efdd70d08602b192a85a48d9d3a9e3f2d716
Jesse <jesseabe.rodrigues@gmail.com>
2024-10-18 22:15:45 -0300

    update

--- 06-pg2aws-etl-ai/xlsx_to_csv.py
+++ 06-pg2aws-etl-ai/xlsx_to_csv.py
@@ -65,3 +65,24 @@ def xlsx_to_csv_xlsx2csv(caminho_xlsx, caminho_para_salvar=None):
     
     except Exception as e:
         print(f"Erro ao converter o arquivo: {e}")
+
+
+#Utilizando Duckdb
+def xlsx_to_csv_duckdb(caminho_xlsx, caminho_para_salvar=None):
+    try:
+        # Se o caminho para salvar não for especificado, cria com o mesmo nome
+        if caminho_para_salvar is None:
+            nome_arquivo = os.path.splitext(os.path.basename(caminho_xlsx))[0]
+            caminho_para_salvar = f"{nome_arquivo}.csv"
+
+        # Usando DuckDB para ler o arquivo Excel e converter para CSV
+        duckdb.query(f"INSTALL 'excel'; LOAD 'excel';")  # Instala e carrega o módulo para XLSX
+        df = duckdb.query(f"SELECT * FROM read_excel('{caminho_xlsx}')").df()
+        
+        # Salvar o DataFrame em CSV
+        df.to_csv(caminho_para_salvar, index=False, encoding='utf-8')
+        
+        print(f"Arquivo convertido e salvo em: {caminho_para_salvar}")
+    
+    except Exception as e:
+        print(f"Erro ao converter o arquivo: {e}")
