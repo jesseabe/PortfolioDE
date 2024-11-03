03c3c6e8652be9a47185c056b936a3b5dab67b46
Jesse <jesseabe.rodrigues@gmail.com>
2024-11-02 22:25:17 -0300

    update

--- 07-etl-dre-completo/src/dashboard.py
+++ 07-etl-dre-completo/src/dashboard.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
+import plotly.express as px
+import matplotlib.pyplot as plt
 
 # Conecte-se ao banco de dados SQLite
 con = sqlite3.connect('data/dre.db')
@@ -12,26 +14,54 @@ df = pd.read_sql('SELECT * FROM dre', con)
 con.close()
 
 #Título do Dashboard
-st.title("Apresentação de Resultados da XJEX")
+st.title("Apresentação de Resultados")
 
-#Título da tabela
-st.write("Cabeçalho dos dados")
-st.write(df.head())
+# Calculando os valores
+RECEITA_BRUTA = df["Receita Bruta"].sum() / 1_000
+IMPOSTOS = df["Impostos"].sum() / 1_000
+RECEITA_LIQUIDA = df["Receita Líquida"].sum() / 1_000
+CMV = df["CMV"].sum() / 1_000
+LUCRO_BRUTO = df["Lucro Bruto"].sum() / 1_000
+DESPESA_OPERACIONAL = df["Despesa Operacional"].sum() / 1_000
+LUCRO_LIQUIDO = df["Lucro Líquido"].sum() / 1_000
 
-import streamlit as st
+# Criando um DataFrame para exibir os dados como uma tabela
+data = {
+    "Descrição": [
+        "Receita Bruta",
+        "Impostos",
+        "Receita Líquida",
+        "CMV",
+        "Lucro Bruto",
+        "Despesa Operacional",
+        "Lucro Líquido"
+    ],
+    "Valor (em milhares)": [
+        RECEITA_BRUTA,
+        IMPOSTOS,
+        RECEITA_LIQUIDA,
+        CMV,
+        LUCRO_BRUTO,
+        DESPESA_OPERACIONAL,
+        LUCRO_LIQUIDO
+    ]
+}
 
-RECEITA_BRUTA = df["Receita Bruta"].sum()
-IMPOSTOS = df["Impostos"].sum()
-RECEITA_LIQUIDA = df["Receita Líquida"].sum()
-CMV = df["CMV"].sum()
-LUCRO_BRUTO = df["Lucro Bruto"].sum()
-DESPESA_OPERACIONAL = df["Despesa Operacional"].sum()
-LUCRO_LÍQUIDO = df["Lucro Líquido"].sum()
+# Convertendo para DataFrame
+df_table = pd.DataFrame(data)
 
-st.write(f'RECEITA BRUTA: {round(RECEITA_BRUTA, 2)}')
-st.write(f'IMPOSTOS: {round(IMPOSTOS, 2)}')
-st.write(f'RECEITA LÍQUIDA: {round(RECEITA_LIQUIDA,2)}')
-st.write(f'CMV: {round(CMV,2)}')
-st.write(f'LUCRO BRUTO: {round(LUCRO_BRUTO,2)}')
-st.write(f'DESPESA OPERACIONAL: {round(DESPESA_OPERACIONAL,2)}')
-st.write(f'LUCRO LÍQUIDO: {round(LUCRO_LÍQUIDO,2)}')
+# Formatando os valores para duas casas decimais com separadores de milhar
+df_table["Valor (em milhares)"] = df_table["Valor (em milhares)"].apply(lambda x: f"{x:,.2f}".replace(",", "X").replace(".", ",").replace("X", "."))
+
+# Exibindo a tabela no Streamlit
+st.table(df_table)
+
+
+# Criando o gráfico de barras para Receita Líquida por Categoria
+plt.figure(figsize=(10, 6))
+plt.bar(df['Categoria'], df['Receita Líquida'], color='skyblue')
+plt.title('Receita Líquida por Categoria')
+plt.xlabel('Categorias')
+plt.ylabel('Receita Líquida (em milhares)')
+plt.xticks(rotation=45)  # Rotaciona os rótulos do eixo X para melhor visualização
+st.pyplot(plt)
